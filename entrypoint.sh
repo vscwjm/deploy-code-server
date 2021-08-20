5f6e9b7a46b9ac8a9e274d9569d163720a6abb12
vscwjm <35147337+vscwjm@users.noreply.github.com>
2021-08-20 21:02:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ if [ "${DOCKER_USER-}" ]; then
     echo "$DOCKER_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/nopasswd >/dev/null
     # Unfortunately we cannot change $HOME as we cannot move any bind mounts
     # nor can we bind mount $HOME into a new home as that requires a privileged container.
-#    sudo usermod --login "$DOCKER_USER" coder
+    sudo usermod --login "$DOCKER_USER" coder
     sudo groupmod -n "$DOCKER_USER" coder
 
 #    sudo sed -i "/coder/d" /etc/sudoers.d/nopasswd
